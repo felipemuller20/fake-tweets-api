d3d1a2e66a9be0095561aace8ef06f0500eda296
felipe <felipeavmuller@gmail.com>
2023-03-30 14:07:42 -0300

    add tweets e users

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Public fake APIs
 
-GET: https://public-apis-mr2bftq1y-felipemuller20.vercel.app/api/twitter-users
+GET: https://public-apis-2iz2ghzso-felipemuller20.vercel.app/api/twitter-users
 
-GET: https://public-apis-mr2bftq1y-felipemuller20.vercel.app/api/tweets
+GET: https://public-apis-2iz2ghzso-felipemuller20.vercel.app/api/tweets
--- src/pages/api/data/tweets.ts
+++ src/pages/api/data/tweets.ts
@@ -1,42 +1,114 @@
+// tá faltando o campo de likes/retweets nos tweets?
+
 export const tweets = [
   {
     id: 1,
     owner: {
       name: 'Trybe',
       username: 'betrybe',
-      profilePicture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+      profilePicture: 'https://pbs.twimg.com/profile_images/1574869347079692296/QpY7cGuV_400x400.jpg',
     },
-    commentsCount: 3,
+    commentsCount: 125,
     tweet: 'Fala tribo! Já visitaram a nova documentação do React? Ficou show!'
   },
   {
     id: 2,
     owner: {
-      name: 'Miguel Silva',
-      username: 'miguelito',
-      profilePicture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+      name: 'Melon Musk',
+      username: 'melonmusk',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/melonmusk.jpeg',
     },
-    commentsCount: 2,
-    tweet: 'Saudades de fazer corujão de CS 1.6.'
+    commentsCount: 5897,
+    tweet: 'O ChatGPT é a nova Skynet.'
   },
   {
     id: 3,
     owner: {
-      name: 'Miguel Silva',
-      username: 'miguelito',
-      profilePicture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+      name: 'Melon Musk',
+      username: 'melonmusk',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/melonmusk.jpeg',
     },
-    commentsCount: 5,
-    tweet: 'Quem não joga da de_dust2 nem gente é!'
+    commentsCount: 1252,
+    tweet: 'Não querendo dizer que eu seja o Neo, de Matrix, mas talvez eu seja...'
   },
   {
     id: 4,
     owner: {
       name: 'Ada Lovelace',
       username: 'loveAda',
-      profilePicture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/ada.jpeg',
     },
-    commentsCount: 3,
+    commentsCount: 5,
     tweet: 'Caramba, programar é bom d+!'
+  },
+  {
+    id: 5,
+    owner: {
+      name: 'Magareth Hamilton',
+      username: 'tothemoon',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/margaret-obama.webp',
+    },
+    commentsCount: 10,
+    tweet: 'Se existisse o ChatGPT na minha época, o homem teria pousado em Marte.'
+  },
+  {
+    id: 6,
+    owner: {
+      name: 'Tinus Lorvalds',
+      username: 'linuxthebest',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/linus.webp',
+    },
+    commentsCount: 2,
+    tweet: 'Feliz aniversário, Linux!'
+  },
+  {
+    id: 7,
+    owner: {
+      name: 'Tinus Lorvalds',
+      username: 'linuxthebest',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/linus.webp',
+    },
+    commentsCount: 15,
+    tweet: 'Falar é fácil, me mostre o código.'
+  },
+  {
+    id: 8,
+    owner: {
+      name: 'Tinus Lorvalds',
+      username: 'linuxthebest',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/gates.png',
+    },
+    commentsCount: 9,
+    tweet: 'Teoria e prática às vezes entram em conflito. E quando isso acontece, a teoria perde. Sempre.'
+  },
+  {
+    id: 9,
+    owner: {
+      name: 'Gill Bates',
+      username: 'windowsisbetter',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/gates.png',
+    },
+    commentsCount: 9878,
+    tweet: 'Feliz aniversário, Windows!'
+  },
+  {
+    id: 10,
+    owner: {
+      name: 'Gill Bates',
+      username: 'windowsisbetter',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/gates.png',
+    },
+    commentsCount: 598,
+    tweet: 'Todos nós precisamos de pessoas que nos darão feedbacks. Assim nós poderemos melhorar.'
+  },
+  {
+    id: 11,
+    owner: {
+      name: 'Gill Bates',
+      username: 'windowsisbetter',
+      profilePicture: 'https://betrybe.github.io/images/fake-twitter/linus.webp',
+    },
+    commentsCount: 256,
+    tweet: 'É bom comemorar o sucesso, mas é mais importante aprender as lições do fracasso.'
   },
 ]
